5ace4679c3d2b4d6534c2438227ff7bd90d0c8f3
Steviggio <steve.mothmora@gmail.com>
2024-02-16 19:02:48 +0100

    Post function added to actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,11 @@
+import { z } from "zod"
+
+const FormSchema = z.object({
+  id: z.string(),
+  image: z.string(),
+  category: z.coerce
+    .number()
+})
 
 export type State = {
   errors?: {
@@ -15,23 +23,48 @@ export async function fetchProject() {
   return data
 }
 
+const PostProject = FormSchema.omit({ id: true })
 
 
 export async function postProject(prevState: State, formData: FormData) {
 
-  const validatedFields = postProject.safeParse({
+  const validatedFields = PostProject.safeParse({
     image: formData.get("image"),
     title: formData.get("title"),
     category: formData.get('category')
   });
 
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return {
-      errors: validatedFields.errors.flatten().fieldErrors,
+      errors: validatedFields.error.flatten().fieldErrors,
       message: "Missing fields. Failed to post a new project.",
     }
   }
 
   const (image, title, category) = validatedFields.data;
+
+  try {
+    const response = await fetch("http://localhost:5678/api/works",
+      {
+        method: "POST",
+        body: formData
+      });
+
+    if (!response) {
+      throw new Error("Failed to post a new project");
+    }
+
+    const responseData = await response.json();
+    return {
+      message: responseData.message,
+    }
+  } catch (error) {
+    return {
+      errors: {
+        message: error.message,
+      },
+      message: "Failed to post a new project"
+    }
+  }
 
 }
