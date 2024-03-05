6b1d40f7003834a746e7555b6f68a9472d27bce2
Steviggio <steve.mothmora@gmail.com>
2024-03-05 19:04:18 +0100

    task: lazy loading components

--- app/ui/homepage/Homepage.tsx
+++ app/ui/homepage/Homepage.tsx
@@ -1,11 +1,22 @@
 import Header from "../Header"
 import Footer from "../Footer"
-import Introduction from "../introduction/introduction"
-import Contact from "../Contact"
-import Gallery from "../galleryElement"
+import dynamic from "next/dynamic"
+import CategoriesList from "../portfolio/categories"
+// import Introduction from "../introduction/introduction"
+// import Contact from "../Contact"
+// import Gallery from "../galleryElement"
 // import { GalleryList } from "../galleryElement"
+// import CategoriesList from "../portfolio/categories"
+
+const Introduction = dynamic(() => import('../introduction/Introduction'))
+const Gallery = dynamic(() => import('../portfolio/gallery'))
+const Contact = dynamic(() => import('../Contact'))
+
 
 export default function Homepage() {
+  const handleSelectCategory = (categoryId: number | null) => {
+    console.log(categoryId)
+  }
   return (
     <>
       <Introduction />
